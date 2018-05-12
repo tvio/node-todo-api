f81d88b1f28512aa5957ef5f0ba537d3df86fafd
Hájek Tomáš <hajek@sukl.cz>
2018-05-12 16:01:10 +0200

    find+insert

---
+++ playground/mongo-find.js
@@ -0,0 +1,41 @@
+// const MongoClient  = require ('mongodb').MongoClient;
+const {MongoClient,ObjectID}  = require ('mongodb'); // vybereme pouze mongo client, to same co nahore plus dalsi
+
+
+MongoClient.connect('mongodb://localhost:27017/Users',(err,client)=>{
+    if (err){
+        console.log('Nelze se dostat na mongo server',err);
+    }
+    console.log('Jsem v mongu');
+    const db = client.db('TodoApp');
+
+//-- find {hotovo:true}
+// db.collection('Todos').find({_id: new ObjectID('5af6efcc9f2c491090a86653')}).toArray((err,results)=>{
+ db.collection('Todos').find({hotovo:false}).toArray((err,results)=>{
+  
+           console.log('Todos');
+
+        if (err)  {
+            console.log('Nelze nacist', err);}
+        console.log(JSON.stringify(results,undefined,2));     
+        
+        
+
+//     client.close();
+//         });
+    
+// });
+
+//   db.collection('Todos').find().count().then((count)=>{
+//         console.log('Todos');
+
+//         if (err)  {
+//             console.log('Nelze nacist', err);}
+//         console.log(`Celkovy pocet TODOS je ${count}`);     
+        
+        
+
+    client.close();
+        });
+    
+});
--- playground/monodb-connect.js
+++ playground/monodb-connect.js
@@ -1,4 +1,6 @@
-const MongoClient  = require ('mongodb').MongoClient;
+// const MongoClient  = require ('mongodb').MongoClient;
+const {MongoClient,ObjectID}  = require ('mongodb'); // vybereme pouze mongo client, to same co nahore plus dalsi
+
 
 MongoClient.connect('mongodb://localhost:27017/Users',(err,client)=>{
     if (err){
@@ -6,29 +8,32 @@ MongoClient.connect('mongodb://localhost:27017/Users',(err,client)=>{
     }
     console.log('Jsem v mongu');
     
-    // const db = client.db('TodoApp');
+    const db = client.db('TodoApp');
     
-    // db.collection('Todos').insertOne({
-    // text: 'Neco musim udelat',
-    // comspleted: false
-    // },(err,result)=>{
-    //     if (err){
-    //         return console.log('Nelze vlozit zaznam',err);
-    //     }
-    //     console.log(JSON.stringify(result.ops,undefined,2));
-    // })
-
-
-    const db = client.db('Users');
-    db.collection('Uzivatele').insertOne({
-        jmeno:'Tomik',
-        vek : 77,
-        lokalita: 'Brno'
+    db.collection('Todos').insertOne({
+    text: 'Neco musim udelat',
+    comspleted: false
     },(err,result)=>{
-         if(!err){
-             console.log(JSON.stringify(result.ops,undefined,2));
-         }   
-    });
+        if (err){
+            return console.log('Nelze vlozit zaznam',err);
+        }
+        console.log(JSON.stringify(result.ops,undefined,2));
+    })
+
+
+    // const db = client.db('Users');
+    // db.collection('Uzivatele').insertOne({
+    //     jmeno:'Tomik',
+    //     vek : 77,
+    //     lokalita: 'Brno'
+    // },(err,result)=>{
+    //      if(!err){
+    //          console.log(JSON.stringify(result.ops,undefined,2));
+    //          console.log(result.ops[0]._id.getTimestamp());
+    //      }  else {
+    //          console.log(err);
+    //      }
+    // });
 
 
 
