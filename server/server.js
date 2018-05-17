465194d874ed3ad080fa43a3fbf3ec33d4017401
tvio <tvio@centrum.cz>
2018-05-17 20:31:39 +0200

    pridana get /todos/id

--- server/server.js
+++ server/server.js
@@ -23,6 +23,9 @@ app.post('/todo',(req,res)=>{
 });
 
 app.get('/todos',(req,res)=>{
+
+
+    
     Todo.find().then((todos)=>{
         res.send({todos,
         code: 'OK',
@@ -37,6 +40,29 @@ app.get('/todos',(req,res)=>{
 app.listen(3000,()=>{
     console.log('Aplikace bezi na portu 3000');
 });
+
+// get jedno tudu
+
+
+
+app.get('/todos/:id',(req,res)=>{
+   var id  = req.params.id;
+
+    // podle me neni potreba vraci callback s errorem
+//  if (!ObjectID.isValid)){
+//      return res.status(404).send();
+//  }
+
+   Todo.findById(id).then((todo)=>{
+       res.send({todo,
+        code: 'OK',
+        datum: new Date});
+       },(e) =>{
+           res.status(400).send(e);
+   });
+});
+
+
 
 module.exports = {app};
 
