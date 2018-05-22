a833082b6631de8fc9eaeeb4537acf64dc88171a
Hájek Tomáš <hajek@sukl.cz>
2018-05-22 16:31:38 +0200

    oprava preklepu v hesle k mongo

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // odkazuje se pouze na jednu db user nebo todos
-mongoose.connect('mongodb://tvio:123HEslo@ds131800.mlab.com:31800/todos');
+mongoose.connect('mongodb://tvio:123Heslo@ds131800.mlab.com:31800/todos');
 
 module.exports = {mongoose};
