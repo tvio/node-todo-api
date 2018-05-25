be8443e0789e9596bfa91a48bcc3249289ffaf9e
Hájek Tomáš <hajek@sukl.cz>
2018-05-25 14:59:25 +0200

    post user

--- server/models/user.js
+++ server/models/user.js
@@ -1,21 +1,41 @@
 var mongoose = require('mongoose');
-
+var validator = require ('validator');
 //user model -- ma na konci s, pokud ne s mongose doplni
- var User = mongoose.model('users', {
-            jmeno: {
+
+
+
+var User = mongoose.model('users', {
+             email: {
                 type: String,
                 minlength: 1,
-                trim: true
-
+                trim: true,
+                required: true,
+                unique: true,
+                //nefunguje email validace
+                validate:{
+                    validator: (value) =>{ 
+                  return validator.isEmail(value);
+                  },
+                  message: '{value} is not valid email'
+                }
             },
-            email: {
+            password: {
                 type: String,
-                minlength: 1,
-                trim: true
+                require: true,
+                minlength:6
+            },
+            tokens: [{
+                access:{
+                  type: String,
+                  required: true  
+                },
+                token:{
+                    type: String,
+                    required: true
 
-            }
+                }
+            }]
 
-        }
-        );
+});
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
 var {mongoose} = require ('./db/mongoose');
 var {Todo} = require ('./models/todo');
-var {Yser} = require ('./models/user');
+var {User} = require ('./models/user');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -123,6 +123,24 @@ app.patch('/todos/:id',(req,res)=>{
     }).catch((e)=>{
         res.status(400).send();
     })
+
+});
+
+// POST / users
+
+app.post('/users',(req,res)=>{
+    var body = _.pick(req.body,['email','password']);
+    var user = new User(body);
+       
+    
+
+    user.save().then((user)=>{
+        res.status(200).send(user);
+      }).catch((e)=>{
+          res.status(400).send(e);
+      })
+        
+    
 
 });
 
