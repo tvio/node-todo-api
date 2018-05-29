2a042e52893a08bb0c826ec96be83bd7089cd7ab
Hájek Tomáš <hajek@sukl.cz>
2018-05-29 17:41:18 +0200

    authentication /get me

--- server/models/user.js
+++ server/models/user.js
@@ -58,6 +58,25 @@ UserSchema.methods.generateAuthToken = function (){
  
 };
 
+UserSchema.statics.findByToken = function (token){
+   var User = this;
+   var decoded;
+
+   try {
+     decoded =  jwt.verify(token,'abc123');    
+   } catch (e) {
+    //  return new Promise ((resolve,reject)=>{
+    //      reject();
+    //  });
+    return  Promise.reject();
+   }
+   return User.findOne({
+     _id: decoded._id,
+     'tokens.token': token,
+     'tokens.access':'auth'
+   });
+};
+
 var User = mongoose.model('users', UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {ObjectID} = require('mongodb');
 var {mongoose} = require ('./db/mongoose');
 var {Todo} = require ('./models/todo');
 var {User} = require ('./models/user');
+var {authenticate} = require('./middleware/atuhenticate');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -145,6 +146,13 @@ app.post('/users',(req,res)=>{
         
     
 
+});
+
+
+
+app.get('/users/me',authenticate, (req,res)=>{
+         res.send(req.user);
+ 
 });
 
 
